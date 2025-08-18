4c86b2b750fd20a8e23cf86de193e419ba26e49e
lockdoor <lockdoor@gmail.com>
2025-08-18 16:23:18 +0700

    feat: use allow host from env

--- django/stockflow/stockflow/settings.py
+++ django/stockflow/stockflow/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "fallback-secret-key-fo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
 # Application definition
 
